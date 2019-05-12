9dbeecd45472d70f63f84192d3b3eba52bc0d0cb
Nino <anthony@delcey.fr>
2019-05-12 21:51:36 +0200

    Add dependencies for Unit Testing

---
+++ app/src/test/java/fr/delcey/mvvm_clean_archi/MainViewModelTest.kt
@@ -0,0 +1,11 @@
+package fr.delcey.mvvm_clean_archi
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MainViewModelTest {
+    @Test
+    fun `hello world test`() {
+        assertTrue(true)
+    }
+}
